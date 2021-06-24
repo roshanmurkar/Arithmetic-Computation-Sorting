1e470b8eb75b1e26ad7f4d39034a4ea838c40377
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-06-24 20:30:22 +0530

    Taking 3 input from user

--- sortProblem.sh
+++ sortProblem.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 
 echo "welcome to arithmetic computation and sorting program"
+
+#uc1-get input
+
+read -p "enter a 1st value:- " a
+read -p "enter a 2nd value:- " b
+read -p "enter a 3rd value:- " c
+
+echo $a,$b,$c
+
