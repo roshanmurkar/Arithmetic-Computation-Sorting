eb1ae4a4c095a70e42d701e7ef0ffb92d416ea58
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-06-25 21:49:12 +0530

    update

---
+++ flipCoinCombination.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -x
+
+declear -A flipValue
+isFlip=0;
+isHead=0;
+isTail=1;
+a=0;
+b=0;
+read -p "Enter how many times you want to flip the coin : " flipCoinTimes
+
+while  [[ isFlip -lt flipCoinTimes ]]
+do
+	flipCoin=$(( RANDOM%2 ))
+	case $flipCoin in
+        	$isHead)
+                	result=Heads
+			((a++))
+                	;;
+        	$isTail)
+                	result=Tails
+			((b++))
+                	;;
+	esac
+	flipValue[$result]=$flipCoin
+	echo $result
+	isFlip=$(( $isFlip + 1 ))
+done
+
+echo "${!flipValue[@]}"
+echo "Heads win $a Times"
+echo "Tails win $b Times"
