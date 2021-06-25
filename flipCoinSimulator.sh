e8eac0412b15ff7799a9aed7a6af7a047e2bc144
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-06-25 09:31:48 +0530

    clr

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,15 +1,1 @@
-#!/bin/bash -x
 
-isHead=0;
-isTail=1;
-flipCheck=$((RANDOM%2))
-
-case $flipCheck in
-	$isHead)
-		result=Heads
-		;;
-	$isTail)
-		result=Tails
-		;;
-esac
-echo $result "is winner"
